06b23431379980b68b618551f8362c9244641212
Kali <davlorenzo@hotmail.com>
2023-01-16 17:33:34 +0100

    Update to configure the environment from colorful-dotfiles

--- install.sh
+++ install.sh
@@ -1,62 +1,6 @@
 #!/usr/bin/sh
-# Instalacion de paquetes
-sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+# Script to install the repo of colorful-dotfiles
+git clone https://github.com/nozerobit/colorful-dotfiles.git && cd colorful-dotfiles
+chmod +x install_clear.sh && ./install_clear.sh
+reboot || systemctl reboot
 
-# Instalacion de bspwm y sxhkd
-git clone https://github.com/baskerville/bspwm.git
-git clone https://github.com/baskerville/sxhkd.git
-cd bspwm/
-make
-sudo make install
-cd ../sxhkd
-make
-sudo make install
-cd ..
-sudo apt install -y bspwm
-
-# Configuracion bspwm
-mkdir ~/.config/bspwm
-mkdir ~/.config/sxhkd
-cp bspwm/examples/bspwmrc ~/.config/bspwm/
-cp sxhkd/examples/sxhkdrc ~/.config/sxhkd/
-cp config_files/sxhkdrc ~/.config/sxhkd/
-chmod +x ~/.config/bspwm/bspwmrc
-chmod +x ~/.config/sxhkd/sxhkdrc
-
-# Creacion archivo bspwm_resize
-mkdir ~/.config/bspwm/scripts
-cp config_files/bspwm_resize ~/.config/bspwm/scripts/bspwm_resize
-chmod +x ~/.config/bspwm/scripts/bspwm_resize
-
-# Instalacion polybar
-sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
-
-git clone --recursive https://github.com/polybar/polybar
-cd polybar/
-mkdir build
-cd build/
-cmake ..
-make -j$(nproc)
-sudo make install
-
-# Instalacion Picom
-sudo apt update -y
-sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
-
-git clone https://github.com/ibhagwan/picom.git
-cd picom/
-git submodule update --init --recursive
-meson --buildtype=release . build
-ninja -C build
-sudo ninja -C build install
-
-# Instalacion Rofi
-sudo apt install -y rofi
-
-
-# Fondo de pantalla
-sudo apt install feh
-# Añadir al fichero bspwmrc
-feh --bg-fill /ruta/imagen
-
-# Configuracion de la polybar
---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+# Script to install automatically tools and configurate the environment installed from colorful-dotfiles
+# Packages and dependencies update
+sudo apt-get update
+
+# Wallpaper configuration on VM init
+sudo cp wallpapers/maker-of-virtual-world-a6-1920x1080.jpg default-wallpaper ~/Pictures/Wallpapers/maker-of-virtual-world-a6-1920x1080.jpg
+default-wallpaper --set ~/Pictures/Wallpapers/maker-of-virtual-world-a6-1920x1080.jpg
+
+# Change polybar to top
+polybar-changer --top
+
+# Tools for VMware Workstation Player (change if you use VirtualBox)
+sudo apt install open-vm-tools open-vm-tools-desktop
+
+# Fix of mouse flicker in vmware guests
+sudo cp config_files/xorg.conf /etc/X11/xorg.conf
+
+# Language configuration (es --> spanish)
+echo "setxkbmap es" >> ~/.config/bspwm/bspwmrc
+
+# Timezone configuration
+sudo timedatectl set-timezone Europe/Madrid
+
+# Chromium installation
+sudo apt-get install chromium
+
+
