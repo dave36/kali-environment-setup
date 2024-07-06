32449e0cd71b694489512504bc33644f809bbb40
David Lorenzo <davlorenzo@hotmail.com>
2024-07-06 23:52:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 # Script to install the repo of colorful-dotfiles
 git clone https://github.com/nqnm/Colorful.git && cd Colorful
-chmod +x install_clear.sh && ./install_clear.sh
+chmod +x install.sh && ./install.sh
 
